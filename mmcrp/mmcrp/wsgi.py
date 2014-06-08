da5723742dd4096c2ef23f32a2fe5e906b12c249
Nick Roberts <narobert@ucsd.edu>
2014-06-07 18:14:42 -0700

    Deploying mysql

--- mmcrp/mmcrp/wsgi.py
+++ mmcrp/mmcrp/wsgi.py
@@ -8,12 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-import sys
-sys.path.append('/Users/narobert/mmcrp/mmcrp/mmcrp/')
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mmcrp.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
