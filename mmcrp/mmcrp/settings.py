42f6e4d92758d55a0282db90bca9db7e1beb845d
Nick Roberts <narobert@ucsd.edu>
2014-06-07 18:13:45 -0700

    Deploying mysql

--- mmcrp/mmcrp/settings.py
+++ mmcrp/mmcrp/settings.py
@@ -9,13 +9,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = '/'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'db_mmcrp',
+    'USER': 'root',
+    'PASSWORD': 'Jr94oo2',
+  }
 }
 
 TIME_ZONE = 'America/Los_Angeles'
@@ -33,6 +37,8 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 MEDIA_URL = '/media/'
+
+STATIC_URL = '/static/'
 
 SECRET_KEY = 'umnk$dy^h6l-e_@90jmk4u%rmb5%qo%ow(hpu3s+r%bi3c6hr6'
 
@@ -85,26 +91,3 @@ LOGGING = {
         },
     }
 }
-
-# Allow all host hosts/domain names for this site
-ALLOWED_HOSTS = ['*']
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-
-DATABASES = { 'default' : dj_database_url.config()}
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# try to load local_settings.py if it exists
-try: 
-	from local_settings import *
-except Exception as e:
-	pass
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
